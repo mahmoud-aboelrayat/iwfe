3091acc428d99461663462b092ccbab36ec4a3d7
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2020-08-12 14:18:07 +0200

    add form

---
+++ static/js/form-main.js
@@ -0,0 +1,21 @@
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // fetch all the forms we want to apply custom style
+        var inputs = document.getElementsByClassName('form-control')
+
+        // loop over each input and watch blue event
+        var validation = Array.prototype.filter.call(inputs, function(input) {
+
+            input.addEventListener('blur', function(event) {
+                // reset
+                input.classList.remove('is-invalid')
+                input.classList.remove('is-valid')
+
+                if (input.checkValidity() === false) {
+                    input.classList.add('is-invalid')
+                }
+            }, false);
+        });
+    }, false);
+})();
