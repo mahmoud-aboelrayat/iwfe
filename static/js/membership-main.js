a923bdf64dd3a7a6c69eb131b3bb26dbeba1bc3d
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2020-08-12 13:22:42 +0200

    add membership

---
+++ static/js/membership-main.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    AOS.init({
+        offset: 120,
+        duration: 800,
+      });
+      
+      const counters = document.querySelectorAll('.counter');
+const speed = 75; // The lower the slower
+
+counters.forEach(counter => {
+	const updateCount = () => {
+		const target = +counter.getAttribute('data-target');
+		const count = +counter.innerText;
+
+		// Lower inc to slow and higher to slow
+		const inc = Math.round(target / speed);
+
+		// console.log(inc);
+		// console.log(count);
+
+		// Check if target is reached
+		if (count < target) {
+			// Add inc to count and output in counter
+			counter.innerText = count + inc;
+			// Call function every ms
+			setTimeout(updateCount, 1);
+		} else {
+			counter.innerText = target;
+		}
+	};
+
+	updateCount();
+});
+
+});
+
